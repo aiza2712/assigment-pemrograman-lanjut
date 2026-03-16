4dfef83998bbd7dbc97c775b9aa7812bf2fcdda9
aizahasya <eizzarihalatex@gmail.com>
2026-03-16 16:45:49 +0700

    Initial commit - assignment0 project

---
+++ assignment0-master/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+    string name;
+
+    cout << "what is your name??";
+    getline (cin, name),
+    cout << "hello, " << name << "!" << endl;
+    
+    return 0;
+}
